52cb44578046a39ff18ca88032c46c27d0617769
Sw1ndlers <lisina2023@icloud.com>
2024-05-17 23:50:16 -0700

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::min, fs, path::PathBuf, sync::Mutex};
+use std::{cmp::min, fs, path::PathBuf, sync::{Arc, Mutex}, thread, time::Duration};
 
 use anyhow::Context;
 use fuzzy_matcher::skim::SkimMatcherV2;
@@ -9,6 +9,10 @@ use rayon::{
     iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator},
     ThreadPoolBuilder,
 };
+
+fn clear_screen() {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+}
 
 struct Matcher {
     query: String,
@@ -28,12 +32,46 @@ impl Matcher {
     }
 }
 
+trait StrExt {
+    fn colorize_matches(&self, indices: Vec<usize>) -> String;
+}
+
+impl StrExt for str {
+    fn colorize_matches(&self, indices: Vec<usize>) -> String {
+        self
+            .chars()
+            .enumerate()
+            .map(|(i, c)| {
+                if indices.contains(&i) {
+                    c.to_string().red().bold().to_string()
+                } else {
+                    c.to_string().normal().to_string()
+                }
+            })
+            .collect()
+    }
+}
+
+fn get_top_matches(matches: &mut Vec<(i64, String)>) -> (Vec<String>, usize) {
+    let slice_index = min(matches.len(), 10);
+    let extra_matches = matches.len() - slice_index;
+
+    matches.sort_by(|a, b| b.0.cmp(&a.0));
+    
+    let matches: Vec<String> = matches[0..slice_index]
+        .iter()
+        .map(|(_, s)| s.clone())
+        .collect();
+
+    (matches, extra_matches)
+}
+
 struct Searcher {
     base_dir: PathBuf,
     matcher: Matcher,
     verbose: bool,
-    matches: Mutex<Vec<(i64, String)>>,
-    last_printed: Mutex<Vec<String>>
+    matches: Arc<Mutex<Vec<(i64, String)>>>,
+    last_printed: Arc<Mutex<Vec<String>>>
 }
 
 impl Searcher {
@@ -41,9 +79,9 @@ impl Searcher {
         Self {
             base_dir,
             verbose,
-            matches: Mutex::new(Vec::new()),
+            matches: Arc::new(Mutex::new(Vec::new())),
             matcher: Matcher::new(query),
-            last_printed: Mutex::new(Vec::new()),
+            last_printed: Arc::new(Mutex::new(Vec::new())),
         }
     }
 
@@ -57,51 +95,79 @@ impl Searcher {
         let parent_dir = relative_path.parent().unwrap().to_str().unwrap();
 
         if let Some((score, indices)) = matcher.fmatch(file_name) {
-            let colored_name: String = file_name
-                .chars()
-                .enumerate()
-                .map(|(i, c)| {
-                    if indices.contains(&i) {
-                        c.to_string().red().bold().to_string()
-                    } else {
-                        c.to_string().normal().to_string()
-                    }
-                })
-                .collect();
+            let colored_name = file_name.colorize_matches(indices);
+            let formatted_string: String;
+
+            if is_dir {
+                formatted_string = format!(".\\{}\\{}", parent_dir, colored_name);
+            } else {
+                formatted_string = format!(".\\{}\\{}", parent_dir, colored_name);
+            }
 
             let mut matches = self.matches.lock().unwrap();
 
-            if is_dir {
-                matches.push((score, format!(".\\{}\\{}", parent_dir, colored_name)));
-            } else {
-                matches.push((score, format!(".\\{}\\{}", parent_dir, colored_name)));
-            }
-
-            let mut last_printed = self.last_printed.lock().unwrap();
-            let slice_index = min(matches.len(), 10);
-
-            let extra_matches = matches.len() - slice_index;
-
-            // matches.sort_by(|a, b| b.0.cmp(&a.0));
-            
-            let matches: Vec<String> = matches[0..slice_index]
-                .iter()
-                .map(|(_, s)| s.clone())
-                .collect();
-
-            if matches == last_printed.to_vec() {
-                print!("\r... {} more matches", extra_matches);
-                return;
-            }
-
-            print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // clear the screen
-            println!("{}", matches.join("\n"));
-
-            *last_printed = matches.clone();
-
-            drop(last_printed);
-            drop(matches);
+            matches.push((score, formatted_string));
         }
+    }
+
+
+
+    fn search(&self, path: &PathBuf) -> anyhow::Result<()> {
+        let matches = Arc::clone(&self.matches);
+        let last_printed = Arc::clone(&self.last_printed);
+
+        let completed_search = Arc::new(Mutex::new(false));
+        let completed_search_clone = Arc::clone(&completed_search);
+
+        thread::spawn(move || {
+            let mut last_printed = last_printed.lock().unwrap();
+
+            loop {
+                let matches_ref = matches.lock().unwrap();
+                let mut matches = matches_ref.clone();
+                let completed_search_clone = completed_search_clone.lock().unwrap();
+
+                if *completed_search_clone {
+                    break;
+                }
+
+                drop(matches_ref);
+
+                let (matches, extra_matches) = get_top_matches(&mut matches);
+
+                if matches == *last_printed {
+                    print!("\r... {} more matches", extra_matches);
+                    continue;
+                }
+                
+                clear_screen();
+                println!("{}", matches.join("\n"));
+    
+                *last_printed = matches;
+            }
+        });
+
+        self.search_directory(path).unwrap();
+        
+        *completed_search.lock().unwrap() = true;
+
+        // print_thread.thread().unpark();
+        
+
+        
+        let matches = self.matches.lock().unwrap();
+        let (matches, extra_matches) = get_top_matches(&mut matches.clone());
+
+        println!("finished");
+
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
+        println!("{}", matches.join("\n"));
+        println!("... {} more matches", extra_matches);
+
+        // println!("Found {} matches", self.matches.lock().unwrap().len());
+
+        Ok(())
     }
 
     fn search_directory(&self, path: &PathBuf) -> anyhow::Result<()> {
@@ -141,13 +207,13 @@ fn main() {
     let verbose = args.contains(&"--verbose".to_string());
 
     ThreadPoolBuilder::new()
-        .num_threads(12)
+        .num_threads(14)
         .build_global()
         .unwrap();
 
     let searcher = Searcher::new(
-        "C:\\".into(),
-        // current_dir.clone(),
+        // "C:\\".into(),
+        current_dir.clone(),
         query.to_string(),
         verbose,
     );
@@ -155,9 +221,9 @@ fn main() {
     let start = std::time::Instant::now();
 
     searcher
-        .search_directory(
-            // &current_dir.clone()
-            &PathBuf::from("C:\\Users"),
+        .search(
+            &current_dir.clone()
+            // &PathBuf::from("C:\\Users"),
         )
         .unwrap();
 
